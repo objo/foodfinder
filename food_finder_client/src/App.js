3964ed3a4f784ade4fc7d758a847840fe1e7dedc
Joe OBrien <jobrien@comresource.com>
2021-02-16 15:38:57 -0500

    adding initial app

---
+++ food_finder_client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>I'm here yall</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
