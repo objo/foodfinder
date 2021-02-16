494d219463dfc773fe3fed506ebbe7eef74998e3
Joe OBrien <jobrien@comresource.com>
2021-02-16 14:01:17 -0500

    configuring geokit with bing key

--- food_finder_api/app/models/vendor.rb
+++ food_finder_api/app/models/vendor.rb
@@ -1,4 +1,6 @@
 class Vendor < ApplicationRecord
+  acts_as_mappable lat_column_name: :latitude,
+                   lng_column_name: :longitude
 
   COMING_SOON = "REQUESTED"
   ACTIVE = "APPROVED"
---
+++ food_finder_api/config/initializers/geokit_config.rb
@@ -0,0 +1,9 @@
+Geokit::default_units = :miles # others :kms, :nms, :meters
+Geokit::default_formula = :sphere
+
+Geokit::Geocoders::request_timeout = 3
+
+Geokit::Geocoders::BingGeocoder.key = 'AikKByeHnBu4Zif9gXLRlFDcsfdmLYCv3uCG53y4Izm7O4l8Xk5YIkqF9wVx1LO2'
+
+Geokit::Geocoders::provider_order = [:bing]
+
